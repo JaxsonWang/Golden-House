e5bb9f7e3b638fdbbd7d1d99aae87cb2f994a01f
Jaxson Wang <i@iiong.com>
2020-09-19 09:18:08 +0800

    🌝 更新内容

--- 2020/0917/Vue3/vue3-router-vuex-jsx/src/views/About.jsx
+++ 2020/0917/Vue3/vue3-router-vuex-jsx/src/views/About.jsx
@@ -1,4 +1,5 @@
 import { mapGetters } from 'vuex'
+import HelloVue from '@/components/HelloVue.jsx'
 
 const styles = {
   about: {
@@ -8,6 +9,9 @@ const styles = {
 
 const About = {
   name: 'About',
+  components: {
+    HelloVue
+  },
   computed: {
     ...mapGetters([
       'systemName'
@@ -16,12 +20,17 @@ const About = {
   methods: {
     onInputChange(event) {
       this.$store.dispatch('app/setSystemName', event.target.value)
+    },
+    helloVueAction(event) {
+      alert('我被点击了！')
+      console.log(event)
     }
   },
   render() {
     return <>
       <div>
         <h3>这是 JSX 页面</h3>
+        <hello-vue msg="这是一个被调用的 JSX 的组件，你可以点击试试看！" onHello-vue={this.helloVueAction}/>
         <label>
           <input type="text" placeholder="系统名称" style={styles.about} value={this.systemName} onInput={this.onInputChange}/>
         </label>
